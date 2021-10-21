4d8015ddbfd4450dbfbda154da5ca2fa91916db3
Júnior Pimenta <juniorpimenta@gmail.com>
2021-10-21 20:59:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function changeSociaMLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${linksSociamMedia[social]}`
-    alert(li.children[0].href)
+    
   }
 }
 changeSociaMLinks()
